34f7cd51cd6179b4f7428348058cb91770ac9448
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-09-28 17:33:33 +0200

    fix in projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -26,23 +26,6 @@ const Projects = () => {
       >
         My Projects
       </motion.h1>
-      <motion.div
-        variants={fadeInAnimationsVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{ once: true }}
-        className="bg-gray-100 mx-10 md:mx-auto max-w-2xl  p-5 rounded-lg border-black/5 border"
-      >
-        <p className="text-center text-lg">test account for projects</p>
-        <div>
-          <p>
-            username: <span className="font-semibold">demo</span>
-          </p>
-          <p>
-            password: <span className="font-semibold">demo123</span>
-          </p>
-        </div>
-      </motion.div>
       {projectsData.map((project, index) => (
         <React.Fragment key={index}>
           <Project {...project} />
